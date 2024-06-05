ec32fa14c5d07fc7028313064173407583f6cc95
FrancescoAntony <francesco31323@gmail.com>
2024-06-05 17:33:53 -0300

    ADS: 01609346 Deploy-js

---
+++ Deploy-js/firebase.js
@@ -0,0 +1,37 @@
+/**
+ * Nome do arquivo: firebase.js
+ * Data de criação: 18/05/2024
+ * Autor: Francesco Antony
+ * Matrícula: 01609346
+ *
+ * Descrição:
+ * Este arquivo JavaScript é responsável por implementar as configurações das variáveis de ambiente (SDK) da hospedagem escolhida (firebase) onde serão realizados os testes.
+ *
+ * Este script é parte o curso de ADS.
+ */
+
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.FIRE_API_KEY,
+    authDomain: process.env.FIRE_AUTH_DOMAIN,
+    databaseURL: process.env.FIRE_DATABASE_URL,
+    projectId: process.env.FIRE_PROJECT_ID,
+    storageBucket: process.env.FIRE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIRE_MESSAGING_SENDER_ID,
+    appId: process.env.FIRE_P_ID,
+    measurementId: process.env.FIRE_MEASUREMENT_ID
+  };
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
